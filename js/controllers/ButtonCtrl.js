71819994a8c86bbb05bf6174efb07f04dac8d7f2
Marcin Grabowiecki <marcin.grabowiecki@cgm.com>
2015-05-11 14:46:44 +0200

    2 zadanko - ng-show na guziku

--- js/controllers/ButtonCtrl.js
+++ js/controllers/ButtonCtrl.js
@@ -1,1 +1,30 @@
 // TODO: ButtonCtrl
+App.controller("ButtonCtrl", [
+    "$scope",
+    function($scope) {
+
+        var klop = {
+            name: "Bob"
+        };
+
+        $scope.klop = klop;
+        $scope.visible=true;
+        var counter=0;
+
+        $scope.toggle = function() {
+            if(counter++%2==0) {
+                $scope.visible = false
+                $scope.class = "alert"
+                $scope.buttonText="bla"
+            } else {
+                $scope.visible = true
+                $scope.class = "xxx"
+                $scope.buttonText="bli"
+
+            }
+
+        };
+
+
+    }
+]);
