8794132b956ceac6b16153dc3366257e2fb2d3b6
Marcin Grabowiecki <marcin.grabowiecki@cgm.com>
2015-05-11 15:59:37 +0200

    3 zadanko - serwis

--- js/services/booksCollection.js
+++ js/services/booksCollection.js
@@ -1,1 +1,32 @@
-//TODO
+App.factory("booksCollections", [
+    function() {
+
+        var books = [
+            {title: "Ksi¹¿ka Telefoniczna"},
+            {title: "Kajko & Kokosz"},
+            {title: "Winnetou"}
+        ];
+
+
+        books.create = function(title) {
+            books.push({title: title});
+        };
+
+        books.update = function(book, title) {
+            book.title = title;
+        };
+
+        books.remove = function(book) {
+            var index = books.indexOf(book);
+            if (index > -1) {
+                books.splice(index, 1);
+            }
+        };
+
+        return books;
+
+
+
+
+    }
+]);
